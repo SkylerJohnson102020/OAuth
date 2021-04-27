35b8032d2438c95a99838009a77d3c29ab0857ba
SkylerJohnson102020 <basseuph@gmail.com>
2021-04-26 17:11:10 -0700

    Fixed bug. Still have bug to fix when logged out AND click on Profile tab.

--- src/Profile.js
+++ src/Profile.js
@@ -3,11 +3,13 @@ import { withAuth0 } from '@auth0/auth0-react';
 
 class Profile extends Component {
   render() {
+      console.log("bump");
     const { user } = this.props.auth0;
     return (
     <>
-         <h3>Hello, {user.name}!</h3>;
-        <p>user.email</p>
+        <h3>Hello, {user.name}!</h3>
+        <p>{user.email}</p>
+        <img src={user.picture} alt="userphoto"></img>
     </>
     )
   }
