30d4585e64460feb9937de64061b93ef40f87f7e
SkylerJohnson102020 <basseuph@gmail.com>
2021-04-28 18:02:41 -0700

    Added createNewBooks function, AddBook componenet added.

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { withAuth0 } from 'react-bootstrap';
+import { Button } from 'react-bootstrap'
 
 class AddBook extends React.Component {
   
-  render()
-    return ()
+  render() {
+    return (
+      <Button>
+
+      </Button>
+      )
+    }
   
 }
+
+export default withAuth0(AddBook);
